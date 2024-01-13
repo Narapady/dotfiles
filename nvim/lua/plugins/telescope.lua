82657f809824be4642df6defd1b17986fc493bea
Narapady <chhuoynarapady@yahoo.com>
2024-01-13 01:50:50 -0800

    feat: add more telescope commands

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,6 +8,68 @@ return {
         build = "make",
       },
     },
+    opts = {
+      defaults = {
+        file_ignore_patterns = { ".git/", "node_modules" },
+        layout_config = {
+          height = 0.90,
+          width = 0.90,
+          preview_cutoff = 0,
+          horizontal = { preview_width = 0.60 },
+          vertical = { width = 0.55, height = 0.9, preview_cutoff = 0 },
+          prompt_position = "top",
+        },
+        path_display = { "smart" },
+        prompt_position = "top",
+        prompt_prefix = "  ",
+        selection_caret = " ",
+        sorting_strategy = "ascending",
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--hidden",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--trim", -- add this value
+        },
+      },
+      pickers = {
+        buffers = {
+          prompt_prefix = "󰸩  ",
+        },
+        commands = {
+          prompt_prefix = "  ",
+          layout_config = {
+            height = 0.63,
+            width = 0.78,
+          },
+        },
+        command_history = {
+          prompt_prefix = "  ",
+          layout_config = {
+            height = 0.63,
+            width = 0.58,
+          },
+        },
+        git_files = {
+          prompt_prefix = "󰊢  ",
+          show_untracked = true,
+        },
+        find_files = {
+          prompt_prefix = "  ",
+          find_command = { "fd", "-H" },
+        },
+        live_grep = {
+          prompt_prefix = "󰱽 ",
+        },
+        grep_string = {
+          prompt_prefix = "󰱽 ",
+        },
+      },
+    },
     keys = {
       -- list of disabled defualt keymaps
       { "<leader>/", false }, --disable grep keymap
@@ -16,6 +78,10 @@ return {
       { "<leader>fb", false }, -- disble buffers keymap
       { "<leader>sd", false }, -- disble show diagnostics keymap
       { "<leader>ff", false }, -- disble file files
+      { "<leader>sC", false }, -- disble find commands
+      { "<leader>sh", false }, -- disble help tags
+      { "<leader>sD", false }, -- disble workspace diagnostics
+      { "<leader>sG", false }, -- disble workspace diagnostics
 
       --  opened buffers
       {
@@ -25,6 +91,23 @@ return {
           builtin.buffers()
         end,
         desc = "Lists opened buffers",
+      },
+      {
+        ";h",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.help_tags()
+        end,
+        desc = "Lists available help tags and opens a new window with the relevant help info",
+      },
+      --  findd commands
+      {
+        ";c",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.commands()
+        end,
+        desc = "Lists available plugins/users commands",
       },
       {
         ";ff",
@@ -83,11 +166,6 @@ return {
       local actions = require("telescope.actions")
 
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-        wrap_results = true,
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
         mappings = {
           n = {},
           i = {
@@ -96,15 +174,6 @@ return {
           },
         },
       })
-      opts.pickers = {
-        diagnostics = {
-          theme = "dropdown",
-          initial_mode = "normal",
-          layout_config = {
-            preview_cutoff = 9999,
-          },
-        },
-      }
       telescope.setup(opts)
       require("telescope").load_extension("fzf")
     end,
